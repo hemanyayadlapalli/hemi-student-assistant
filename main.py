879e79000d7949e13a0312ab57ac610ed40ead2a
hemanyayadlapalli <ifwhemi@gmail.com>
2026-09-08 22:41:54 +0530

    mainmenu created

--- main.py
+++ main.py
@@ -5,4 +5,14 @@ print("=" * 35)
 user_name = input("Enter your name: ")
 
 print("\nWelcome,", user_name + "!")
-print("HEMI is ready to help you study.")
+print("\nWhat would you like to do?")
+
+print("1. Study Tasks")
+print("2. Grade Calculator")
+print("3. Study Timer")
+print("4. Quiz")
+print("5. Exit")
+
+choice = input("\nEnter your choice: ")
+
+print("\nYou selected option", choice)
